1ceacbc5b3a7b7e723a25d41e47207ff9937dabc
Mohit50cyber <68845963+Mohit50cyber@users.noreply.github.com>
2022-07-15 21:48:01 +0530

    First commit

---
+++ src/main/java/mohit/MohitJenkinsApplication.java
@@ -0,0 +1,26 @@
+package mohit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class MohitJenkinsApplication {
+
+
+	public static Logger logger= LoggerFactory.getLogger(SpringApplication.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("App started..........");
+	}
+
+	public static void main(String[] args) {
+		logger.info("App executedd.................");
+		SpringApplication.run(MohitJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mohit/MohitJenkinsApplicationTests.java
@@ -0,0 +1,26 @@
+package mohit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MohitJenkinsApplicationTests {
+
+	public static Logger logger= LoggerFactory.getLogger(SpringApplication.class);
+	@Test
+	public void contextLoads() {
+		logger.info("Test case executing...........");
+		assertEquals(true,true);
+	}
+
+
+
+}
